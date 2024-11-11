c9e736cdb9506fdc6ab9db26cbf9f2e8e0658d2c
admin <ravii.chunduru@gmail.com>
2024-11-11 19:36:48 +0530

    Updated CucumberTestRunner.java so that it can run scenarios in parallel

--- src/test/java/com/mycompany/TestRunner/CucumberTestRunner.java
+++ src/test/java/com/mycompany/TestRunner/CucumberTestRunner.java
@@ -2,6 +2,7 @@ package com.mycompany.TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(tags = "",
   features = "src/test/resources/features", // Path to your feature files
@@ -9,5 +10,10 @@ import io.cucumber.testng.CucumberOptions;
   plugin = {"pretty", "html:target/cucumber-reports.html"} // Optional: report plugins
 )
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
-  // This class serves as the Cucumber TestNG test runner
+
+  @Override
+  @DataProvider(parallel = true)
+  public Object[][] scenarios() {
+    return super.scenarios();
+  }
 }
